cc24af00e79bfd967624bf1950fa76cb9a84792b
PrimalDev <71993933+spezzuti@users.noreply.github.com>
2026-02-27 12:59:26 -0500

    Update vacuum.py

--- custom_components/deebot/vacuum.py
+++ custom_components/deebot/vacuum.py
@@ -4,15 +4,10 @@ from functools import partial
 
 from homeassistant.components.vacuum import (
     ATTR_STATUS,
-    STATE_CLEANING,
-    STATE_DOCKED,
-    STATE_ERROR,
-    STATE_IDLE,
-    STATE_PAUSED,
-    STATE_RETURNING,
+    StateVacuumEntity,
+    VacuumActivity,
+    VacuumEntityFeature,
 )
-
-from homeassistant.components.vacuum import StateVacuumEntity, VacuumEntityFeature
 from homeassistant.helpers.icon import icon_for_battery_level
 
 from . import ECOVACS_DEVICES, CONF_SUPPORTED_FEATURES, ECOVACS_CONFIG
@@ -22,16 +17,18 @@ _LOGGER = logging.getLogger(__name__)
 ATTR_ERROR = "error"
 ATTR_COMPONENT_PREFIX = "component_"
 
-STATE_MAP = {
-    "cleaning": STATE_CLEANING,
-    "auto": STATE_CLEANING,
-    "spot_area": STATE_CLEANING,
-    "charging": STATE_DOCKED,
-    "idle": STATE_DOCKED,
-    "pause": STATE_PAUSED,
-    "returning": STATE_RETURNING,
-    "stop": STATE_IDLE,
+# Map Ozmo/Deebot raw statuses to HA VacuumActivity
+ACTIVITY_MAP = {
+    "cleaning": VacuumActivity.CLEANING,
+    "auto": VacuumActivity.CLEANING,
+    "spot_area": VacuumActivity.CLEANING,
+    "charging": VacuumActivity.DOCKED,
+    "idle": VacuumActivity.DOCKED,
+    "pause": VacuumActivity.PAUSED,
+    "returning": VacuumActivity.RETURNING,
+    "stop": VacuumActivity.IDLE,
 }
+
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the Ecovacs vacuums."""
@@ -46,22 +43,33 @@ class EcovacsDeebotVacuum(StateVacuumEntity):
     """Ecovacs Vacuums such as Deebot."""
 
     def __init__(self, device, config):
+        """Initialize the Ecovacs Vacuum."""
         _LOGGER.debug("CONFIG: %s", str(config))
 
-        """Initialize the Ecovacs Vacuum."""
         self.device = device
         self.device.connect_and_wait_until_ready()
+
         if self.device.vacuum.get("nick", None) is not None:
             self._name = "{}".format(self.device.vacuum["nick"])
         else:
             # In case there is no nickname defined, use the device id
             self._name = "{}".format(self.device.vacuum["did"])
 
-        self.clean_mode = 'auto'
-        self._fan_speed = 'normal'
+        self.clean_mode = "auto"
+        self._fan_speed = "normal"
         self._error = None
-        self._supported_features = config[CONF_SUPPORTED_FEATURES]
-        _LOGGER.debug("Vacuum initialized: %s with features: %d", self.name, self._supported_features)
+
+        # Bitmask from __init__.py (VacuumEntityFeature)
+        self._supported_features = config.get(CONF_SUPPORTED_FEATURES, 0)
+
+        # Ensure STATE is present for modern vacuum UI behavior
+        self._supported_features |= VacuumEntityFeature.STATE
+
+        _LOGGER.debug(
+            "Vacuum initialized: %s with features: %s",
+            self.name,
+            self._supported_features,
+        )
 
     async def async_added_to_hass(self) -> None:
         """Set up the event listeners now that hass is ready."""
@@ -72,19 +80,13 @@ class EcovacsDeebotVacuum(StateVacuumEntity):
         self.device.errorEvents.subscribe(self.on_error)
 
     def on_error(self, error):
-        """Handle an error event from the robot.
-
-        This will not change the entity's state. If the error caused the state
-        to change, that will come through as a separate on_status event
-        """
+        """Handle an error event from the robot."""
         if error == "no_error":
             self._error = None
         else:
             self._error = error
 
-        self.hass.bus.fire(
-            "ecovacs_error", {"entity_id": self.entity_id, "error": error}
-        )
+        self.hass.bus.fire("ecovacs_error", {"entity_id": self.entity_id, "error": error})
         self.schedule_update_ha_state()
 
     def on_fan_change(self, fan_speed):
@@ -98,7 +100,7 @@ class EcovacsDeebotVacuum(StateVacuumEntity):
 
     @property
     def unique_id(self) -> str:
-        """Return an unique ID."""
+        """Return a unique ID."""
         return self.device.vacuum.get("did", None)
 
     @property
@@ -122,23 +124,25 @@ class EcovacsDeebotVacuum(StateVacuumEntity):
         return self._supported_features
 
     @property
-    def state(self):
-        try:
-            return STATE_MAP[self.device.vacuum_status]
-        except KeyError:
-            return STATE_ERROR
+    def activity(self) -> VacuumActivity:
+        """Return the current activity."""
+        return ACTIVITY_MAP.get(self.device.vacuum_status, VacuumActivity.ERROR)
+
+    @property
+    def state(self) -> str:
+        """Legacy vacuum state for StateVacuumEntity compatibility."""
+        return self.activity.value
 
     @property
     def status(self):
-        """Return the status of the vacuum cleaner."""
+        """Return the raw status of the vacuum cleaner."""
         return self.device.vacuum_status
 
     def return_to_base(self, **kwargs):
         """Set the vacuum cleaner to return to the dock."""
         from ozmo import Charge
-
         self.device.run(Charge())
-	
+
     @property
     def battery_icon(self):
         """Return the battery icon for the vacuum cleaner."""
@@ -148,23 +152,21 @@ class EcovacsDeebotVacuum(StateVacuumEntity):
 
     @property
     def battery_level(self):
-        """Return the battery level of the vacuum cleaner."""
+        """Return the battery level of the vacuum cleaner (0-100)."""
         if self.device.battery_status is not None:
             return self.device.battery_status * 100
-
-        return super().battery_level
+        return None
 
     @property
     def fan_speed(self):
         """Return the fan speed of the vacuum cleaner."""
         if bool(self.supported_features & VacuumEntityFeature.FAN_SPEED):
             return self._fan_speed
-        return 'normal'
+        return "normal"
 
     def set_fan_speed(self, fan_speed, **kwargs):
         """Set fan speed."""
         from ozmo import SetCleanSpeed
-
         self.device.run(SetCleanSpeed(fan_speed))
         self._fan_speed = fan_speed
 
@@ -175,15 +177,13 @@ class EcovacsDeebotVacuum(StateVacuumEntity):
     def fan_speed_list(self):
         """Get the list of available fan speed steps of the vacuum cleaner."""
         from ozmo import FAN_SPEED_NORMAL, FAN_SPEED_HIGH
-
         return [FAN_SPEED_NORMAL, FAN_SPEED_HIGH]
 
     def turn_on(self, **kwargs):
         """Turn the vacuum on and start cleaning."""
-        from ozmo import Clean, SpotArea
-
-        self.clean_mode = 'auto'
-        self.device.run(Clean(mode=self.clean_mode, speed=self.fan_speed, action='start'))
+        from ozmo import Clean
+        self.clean_mode = "auto"
+        self.device.run(Clean(mode=self.clean_mode, speed=self.fan_speed, action="start"))
 
     def start(self):
         self.turn_on()
@@ -196,26 +196,23 @@ class EcovacsDeebotVacuum(StateVacuumEntity):
     def stop(self, **kwargs):
         """Stop the vacuum cleaner."""
         from ozmo import Clean
-
-        self.device.run(Clean(mode=self.clean_mode, speed=self.fan_speed, action='stop'))
+        self.device.run(Clean(mode=self.clean_mode, speed=self.fan_speed, action="stop"))
 
     def pause(self, **kwargs):
-        """Stop the vacuum cleaner."""
+        """Pause the vacuum cleaner."""
         from ozmo import Clean
-
-        self.device.run(Clean(mode=self.clean_mode, speed=self.fan_speed, action='pause'))
+        self.device.run(Clean(mode=self.clean_mode, speed=self.fan_speed, action="pause"))
 
     def resume(self, **kwargs):
-        """Stop the vacuum cleaner."""
+        """Resume the vacuum cleaner."""
         from ozmo import Clean
-
-        self.device.run(Clean(mode=self.clean_mode, speed=self.fan_speed, action='resume'))
+        self.device.run(Clean(mode=self.clean_mode, speed=self.fan_speed, action="resume"))
 
     def start_pause(self, **kwargs):
         """Start, pause or resume the cleaning task."""
-        if self.device.vacuum_status == 'pause':
+        if self.device.vacuum_status == "pause":
             self.resume()
-        elif self.device.vacuum_status != 'pause':
+        else:
             self.pause()
 
     async def async_start_pause(self, **kwargs):
@@ -225,87 +222,61 @@ class EcovacsDeebotVacuum(StateVacuumEntity):
     def clean_spot(self, **kwargs):
         """Perform a spot clean-up."""
         from ozmo import Clean
-
-        self.clean_mode = 'spot'
-        self.device.run(Clean(mode=self.clean_mode, speed=self.fan_speed, action='start'))
+        self.clean_mode = "spot"
+        self.device.run(Clean(mode=self.clean_mode, speed=self.fan_speed, action="start"))
 
     def locate(self, **kwargs):
         """Locate the vacuum cleaner."""
         from ozmo import PlaySound
-
         self.device.run(PlaySound())
 
     def send_command(self, command, params=None, **kwargs):
         """Send a command to a vacuum cleaner."""
-
-        """
-        {
-          "entity_id": "vacuum.<ID>",
-          "command": "spot_area",
-          "params" : {
-            "area": "0,2"
-          }
-        }
-
-        or
-
-        {
-          "entity_id": "vacuum.<ID>",
-          "command": "spot_area",
-          "params" : {
-            "map": "1580.0,-4087.0,3833.0,-7525.0"
-          }
-        }
-
-		or
-
-        Send command to edge clean.
-
-        {
-          "entity_id": "vacuum.<ID>",
-          "command": "clean_edge",
-        }
-        """
-
         from ozmo import VacBotCommand, Edge
 
-        if command == 'clean_edge':
+        params = params or {}
+
+        if command == "clean_edge":
             self.device.run(Edge())
+            return
 
-        if command == 'spot_area':
-            if 'area' in params:
-                return self.clean_area(params['area'])
-            elif 'map' in params:
-                return self.clean_map(params['map'])
+        if command == "spot_area":
+            if "area" in params:
+                return self.clean_area(params["area"])
+            if "map" in params:
+                return self.clean_map(params["map"])
 
-        if command == 'set_water_level':
-            return self.set_water_level(params['level'])
+        if command == "set_water_level":
+            return self.set_water_level(params.get("level"))
 
         self.device.run(VacBotCommand(command, params))
 
-    def clean_map(self, map):
+    def clean_map(self, map_position):
         from ozmo import Clean, SpotArea
 
-        if not map:
-            self.clean_mode = 'auto'
-            self.device.run(Clean(mode=self.clean_mode, speed=self.fan_speed, action='start'))
+        if not map_position:
+            self.clean_mode = "auto"
+            self.device.run(Clean(mode=self.clean_mode, speed=self.fan_speed, action="start"))
         else:
-            self.clean_mode = 'spot_area'
-            self.device.run(SpotArea(map_position=map, speed=self.fan_speed, action='start'))
+            self.clean_mode = "spot_area"
+            self.device.run(
+                SpotArea(map_position=map_position, speed=self.fan_speed, action="start")
+            )
 
     def clean_area(self, area):
         from ozmo import Clean, SpotArea
 
         if not area:
-            self.clean_mode = 'auto'
-            self.device.run(Clean(mode=self.clean_mode, speed=self.fan_speed, action='start'))
+            self.clean_mode = "auto"
+            self.device.run(Clean(mode=self.clean_mode, speed=self.fan_speed, action="start"))
         else:
-            self.clean_mode = 'spot_area'
-            self.device.run(SpotArea(area=area, speed=self.fan_speed, action='start'))
+            self.clean_mode = "spot_area"
+            self.device.run(SpotArea(area=area, speed=self.fan_speed, action="start"))
 
     def set_water_level(self, level):
         from ozmo import SetWaterLevel
-
+        if level is None:
+            return
         self.device.run(SetWaterLevel(level=level))
 
     @property
@@ -319,6 +290,7 @@ class EcovacsDeebotVacuum(StateVacuumEntity):
             data[attr_name] = int(val * 100)
 
         data["clean_mode"] = self.clean_mode
-        data[ATTR_STATUS] = self.state
+        # Keep ATTR_STATUS for any legacy cards/automations expecting it
+        data[ATTR_STATUS] = self.activity.value
 
         return data
